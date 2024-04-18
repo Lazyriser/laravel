434e231ec4d812fed4fafbda1dedff03b31f7c4b
Sepelite <lazyriser@gmail.com>
2024-04-18 22:08:54 +0300

    modified:   database/seeders/NewsSeeder.php

--- database/seeders/NewsSeeder.php
+++ database/seeders/NewsSeeder.php
@@ -7,11 +7,15 @@ use Illuminate\Database\Seeder;
 
 class NewsSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
-    {
-        //
-    }
+	/**
+	 * Run the database seeds.
+	 */
+	public function run(): void
+	{
+		DB::table('news')->insert([
+			'name' => Str::random(10),
+			'email' => Str::random(10) . '@gmail.com',
+			'password' => Hash::make('password'),
+		]);
+	}
 }
